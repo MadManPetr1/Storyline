560a367bb396ed2849e8850b84d3c7bc4d18413c
MadManPetr1 <galaxyheaven@post.cz>
2025-06-19 17:42:36 +0200

    Added React entrypoint files

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Storyline is live.</h1>
+      {/* Add your components and logic here */}
+    </div>
+  );
+}
+
+export default App;
